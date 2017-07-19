52f904c083c8ef4f20e08112947aa99bb1e91d17
Radim Kazlepka <radim.kazlepka@tieto.com>
2017-07-19 11:31:53 +0200

    README.md update, project minimum added

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# skeleton
+#Skeleton
+
+Skeleton is the boilerplate for quick rup-up any responsive project
+
+## Getting started
+
+### Install Global Dependencies
+  * [Node.js](http://nodejs.org)
+  * [bower](http://bower.io): `[sudo] npm install bower -g`
+  * [bower installer](https://github.com/rquadling/bower-installer): `[sudo] npm install bower-installer -g`
+  * [gulp](http://gulpjs.com/): `[sudo] npm install gulp -g`
+
+### Install Local Dependencies
+  * cd to project folder
+  * run `[sudo] npm install` (first time users)
+  * run `bower-installer` (first time users)
+  * run `gulp` (to watch and compile sass files)
+
+## FAQ
+
+### Windows users
+
+To use Bower on Windows, you must install Git for Windows correctly. Be sure to check the options shown below:
+
+  * Use Git from the Windows Command Prompt
+  * Checkout Windows-style, commit Unix-style line endings
+
+Download link [Git for Windows](http://git-for-windows.github.io/)
+
+### Git
+
+If you have problem to get bower packages with this error:
+
+```
+bower ECMDERR	Failed to execute "git ls-remote --tags --heads git://github.com/...", exit code of #128 fatal: unable to connect to github.com: github.com[0: 192.30.252.128]: errno=No error
+```
+
+You have to change the protocol by running this command:
+```
+git config --global url."https://".insteadOf git://
+```
---
+++ gulpfile.js
@@ -0,0 +1,148 @@
+// Include gulp
+var gulp = require('gulp');
+
+// Include plugins
+var browserSync = require('browser-sync').create();
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
+var gutil = require('gulp-util');
+var cleanCSS = require('gulp-clean-css');
+var spritesmith = require('gulp.spritesmith');
+var buffer = require('vinyl-buffer');
+var imagemin = require('gulp-imagemin');
+var merge = require('merge-stream');
+var concat = require('gulp-concat');
+var clean = require('gulp-clean');
+var sourcemaps = require('gulp-sourcemaps');
+
+var onError = function (err) {
+  gutil.beep();
+  console.log(err.toString());
+  this.emit('end');
+};
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: ''
+    },
+  })
+})
+
+// General styles
+gulp.task('sass', function() {
+    return gulp.src('src/sass/main.scss')
+        .pipe(plumber({
+          errorHandler: onError
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(sass())
+        .pipe(cleanCSS({
+                rebase: false
+            })
+        )
+        .pipe(rename('main.css'))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist/css/'))
+        .pipe(browserSync.reload({
+          stream: true
+        }));
+});
+
+// Creation of the sprite image
+gulp.task('sprite', function(){
+    var spriteData = gulp.src('src/sprite/*.png')
+        .pipe(spritesmith({
+            imgName: 'sprite.png',
+            cssName: 'sprite.scss'
+        })
+    );
+
+    var imgStream = spriteData.img
+        .pipe(buffer())
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img/'));
+
+    // Pipe CSS stream through CSS optimizer and onto disk
+    var cssStream = spriteData.css
+        .pipe(gulp.dest('src/sass/general'));
+
+    // Return a merged stream to handle both `end` events
+    return merge(imgStream, cssStream)
+        .pipe(browserSync.reload({
+          stream: true
+        }));
+});
+
+// Bootstrap
+gulp.task('bootstrap_sass', function(){
+    var bootstrap = gulp.src('src/sass/libs/bootstrap-sass/bootstrap.scss')
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+        .pipe(sass())
+        .pipe(cleanCSS())
+        .pipe(rename('bootstrap.css'))
+        .pipe(gulp.dest('dist/libs/bootstrap-sass/css/'));
+
+    var bootstrapTheme = gulp.src('src/sass/libs/bootstrap-sass/bootstrap-theme.scss')
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+        .pipe(sass())
+        .pipe(cleanCSS())
+        .pipe(rename('bootstrap-theme.css'))
+        .pipe(gulp.dest('dist/libs/bootstrap-sass/css/'));
+
+    // Return a merged stream to handle both `end` events
+    return merge(bootstrap, bootstrapTheme)
+        .pipe(browserSync.reload({
+          stream: true
+        }));
+});
+
+gulp.task("bootstrap_js", function(){
+    var path = 'src/libs/bootstrap-sass/assets/javascripts/bootstrap/';
+    var modules = [
+        //path + 'transition.js',
+        //path + 'alert.js',
+        //path + 'button.js',
+        //path + 'carousel.js',
+        //path + 'collapse.js',
+        //path + 'dropdown.js',
+        //path + 'modal.js',
+        //path + 'tooltip.js',
+        //path + 'popover.js',
+        //path + 'scrollspy.js',
+        //path + 'tab.js',
+        //path + 'affix.js'
+    ];
+
+    if(modules.length > 0)
+    {
+        return gulp.src(modules)
+            .pipe(concat('bootstrap.js'))
+            .pipe(gulp.dest('dist/libs/bootstrap-sass/js/'));
+    }
+    else
+    {
+        return gulp.src('dist/libs/bootstrap-sass/js/bootstrap.js', {read: false})
+            .pipe(clean());
+    }
+});
+
+gulp.task('bootstrap', ['bootstrap_sass','bootstrap_js']);
+
+gulp.task('watch', ['browserSync', 'sprite', 'sass'], function() {
+    gulp.watch('src/sass/**/*.scss', ['sass']);
+    gulp.watch('src/sprite/**/*.png', ['sprite', 'sass']);
+    gulp.watch('src/js/**/*.js', browserSync.reload);
+    gulp.watch('*.html', browserSync.reload);
+
+    // Bootstrap custom watch task
+    gulp.watch('src/sass/libs/**/*.scss', ['bootstrap_sass']);
+});
+
+// Default task
+gulp.task('default', ['bootstrap', 'watch']);
